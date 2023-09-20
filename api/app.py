8d06011f089e4d1e24b0d015c6df705737af9772
Alan Cordoba <105000699+AlanC21@users.noreply.github.com>
2023-09-20 13:08:56 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -26,7 +26,7 @@ def index():
     
     status_mapping = {
         "Not Started": "No iniciado",
-        "First Half": "Primer tiempo",
+        "First half": "Primer tiempo",
         "HalfTime": "Medio tiempo",
         "Second half": "Segundo tiempo",
         "Full Time": "Finalizado",
